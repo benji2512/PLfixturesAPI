3647f1d0589b8737a90e9c1bb52cf0cbb2ead860
Ben Clarke <benthedev@outlook.com>
2021-07-07 00:20:25 +0100

    Adding some date and time field for each match

--- utils/siteScraper.py
+++ utils/siteScraper.py
@@ -1,4 +1,4 @@
-import requests
+import requests, datetime
 
 def main(gameweek):
     teams = {1:"Arsenal", 2:"Aston Villa", 3:"Brentford", 4:"Brighton and Hove Albion", 5:"Burnley", 6:"Chelsea", 7:"Crystal Palace", 8:"Everton", 9:"Leeds United", 10:"Leicester City", 11:"Liverpool", 12:"Manchester City", 13:"Manchester United", 14:"Newcastle United", 15:"Norwich City", 16:"Southampton", 17:"Tottenham Hotspur", 18:"Watford", 19:"West Ham United", 20:"Wolverhampton Wanderers"}
@@ -10,6 +10,10 @@ def main(gameweek):
         if result[i]["event"] == gameweek:
             team_a = result[i]["team_a"]
             team_h = result[i]["team_h"]
+            kickoffDateTime = result[i]["kickoff_time"]
+            kickoffDateTime = datetime.datetime.fromisoformat(kickoffDateTime[:-1])
+            kickoffDate = "{0}-{1}-{2}".format(kickoffDateTime.day, kickoffDateTime.month, kickoffDateTime.year)
+            kickoffTime2 = kickoffDateTime.time()
             if team_a in teams.keys():
                 away = team_a
             else:
@@ -18,7 +22,7 @@ def main(gameweek):
                 home = team_h
             else:
                 home = "Not in dict"
-            game = "{0} vs {1}".format(teams[home], teams[away])
+            game = "{0} vs {1} on {2} at {3}".format(teams[home], teams[away], kickoffDate, kickoffTime2)
             print(game)
             j+=1
         i+=1
