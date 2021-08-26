f39af6df9bbaaf3112cc5d739b8da57864d8988b
ben <benthedev@outlook.com>
2021-08-26 00:59:01 +0000

    redacted mongouri

--- OpenFaasFunctions/matchbydate/handler.py
+++ OpenFaasFunctions/matchbydate/handler.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 def handle(date):
-    client = MongoClient("mongodb+srv://benthedev:B3n4i%402516@cluster0.50ibf.mongodb.net/PLFixtures?retryWrites=true&w=majority")
+    client = MongoClient("mongodb+srv://benthedev:{PASSWORD}@cluster0.50ibf.mongodb.net/PLFixtures?retryWrites=true&w=majority")
     db = client.PLFixtures
 
     cursor = db.Gameweeks.find({"date":date})
