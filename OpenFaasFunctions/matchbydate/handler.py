c363ae741bfbd13eb2f0335a6f6960f1c56c0e4c
ben <benthedev@outlook.com>
2021-08-26 00:36:11 +0000

    Added first functio to OpenFaaS, update joplin on this change tomorrow

--- OpenFaasFunctions/matchbydate/handler.py
+++ OpenFaasFunctions/matchbydate/handler.py
@@ -1,15 +1,10 @@
-import requests
 from pymongo import MongoClient
-from pprint import pprint
-
-def main():
+def handle(date):
     client = MongoClient("mongodb+srv://benthedev:B3n4i%402516@cluster0.50ibf.mongodb.net/PLFixtures?retryWrites=true&w=majority")
     db = client.PLFixtures
 
-    cursor = db.Gameweeks.find({"match":"Manchester United vs Chelsea"})
-
+    cursor = db.Gameweeks.find({"date":date})
+    games = []
     for gw in cursor:
-        pprint(gw)
-
-if __name__ == "__main__":
-    main()
+        games.append(gw)
+    return games
